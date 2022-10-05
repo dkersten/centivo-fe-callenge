3e381a538561c44eccc5832bcf4b0ac53d6e0323
dkersten <dkersten1994@gmail.com>
2022-10-05 16:53:05 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { render } from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+render(<App />, document.getElementById("root"));
